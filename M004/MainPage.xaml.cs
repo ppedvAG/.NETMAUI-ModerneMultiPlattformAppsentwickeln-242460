8eafff2c67e6a0a1082b0e2c09c06ffa1edeb4e7
ppedvLukasK <lukask@ppedv.de>
2024-10-07 13:54:40 +0200

    M004

---
+++ M004/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿namespace M004;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		BackendPicker.ItemsSource = Enumerable.Range(0, 10).ToList();
+		LV.ItemsSource = new List<Person>()
+		{
+			new Person("Max", "Mustermann"),
+			new Person("Max", "Muster2")
+		};
+	}
+
+	private void KlickClicked(object sender, EventArgs e)
+	{
+		Ausgabefeld.Text = Eingabefeld.Text;
+	}
+
+	private void Eingabefeld_TextChanged(object sender, TextChangedEventArgs e)
+	{
+		Ausgabefeld.Text = Eingabefeld.Text;
+	}
+
+	private void SearchBar_SearchButtonPressed(object sender, EventArgs e)
+	{
+
+	}
+
+	private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+	{
+
+	}
+
+	private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+	{
+		Ausgabefeld.FontSize = (double) e.NewValue;
+	}
+
+	private void Stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+	{
+		StepperValue.Text = e.NewValue.ToString();
+	}
+}
+
+public class Person
+{
+	public Person(string vorname, string nachname)
+	{
+		Vorname = vorname;
+		Nachname = nachname;
+	}
+
+	public string Vorname { get; set; }
+
+	public string Nachname { get; set; }
+}
