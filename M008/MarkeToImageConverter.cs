b45c82aca3a9bfd02d682f1f34fcf9ace3e0660b
ppedvLukasK <lukask@ppedv.de>
2024-10-08 15:09:09 +0200

    M008

---
+++ M008/MarkeToImageConverter.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace M008;
+
+public class MarkeToImageConverter : IValueConverter
+{
+	public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+		((FahrzeugMarke) value).ToString().ToLower() + ".png";
+
+	public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => 
+		throw new NotImplementedException();
+}
